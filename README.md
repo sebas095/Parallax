7ee6a2f6dcdcd48a6f600d8b1760a3b9f78ced2b
Sebastian Duque <sebas_tian_95@hotmail.com>
2016-04-23 00:50:02 -0500

    Init Test

---
+++ README.md
@@ -0,0 +1,2 @@
+# PARALLAX
+Pruebas sobre el efecto parallax usando [Stellar.js](http://markdalgleish.com/projects/stellar.js/)
---
+++ js/init.js
@@ -0,0 +1,9 @@
+$(function() {
+  $.stellar({
+    horizontalScrolling: false,
+    responsive: true,
+    horizontalOffset: 0,
+    positionProperty: 'transform',
+    verticalOffset: 0
+  });
+});
